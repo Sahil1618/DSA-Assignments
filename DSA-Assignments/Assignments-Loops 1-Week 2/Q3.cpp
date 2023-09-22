182ac69e57c1c544ea78ae7e1b9617bcc9172089
Sahil1618 <ss8876584@gmail.com>
2023-09-22 14:34:58 +0530

    Loops 1 Week 2

---
+++ DSA-Assignments/Assignments-Loops 1-Week 2/Q3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int n ;
+    cout <<"Enter a integer you want to print table of : "<<endl;
+    cin >> n ; 
+    cout<< endl;
+	for (int i = 1; i <= 10; i++) {
+		cout << n*i << endl;
+	}
+
+}
